c1df48b85a5498b8cb1c76edebc73f89608c0894
ekhel <blackmanskill@gmail.com>
2020-03-30 12:11:34 +0900

    Related_Fields

--- appbackend/api/serializers.py
+++ appbackend/api/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from ..models import Penderita, Kabupaten
 
-class PenderitaSerializers(serializers.ModelSerializer):
+class PenderitaSerializers(serializers.HyperlinkedModelSerializer):
+    lokasi = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Penderita
         fields = (
