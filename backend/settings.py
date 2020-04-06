964abddb32187d3bf3c9c9abb121bc592ea8b571
ekhel <blackmanskill@gmail.com>
2020-04-06 14:40:58 +0900

    drfpage

--- backend/settings.py
+++ backend/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 50
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
