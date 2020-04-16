1d15c71718f7723a1b79b8a8f5a31509bf8ea53c
ekhel <blackmanskill@gmail.com>
2020-04-16 14:48:35 +0900

    DRF

--- backend/settings.py
+++ backend/settings.py
@@ -125,11 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
-}
-
 CORS_ORIGIN_ALLOW_ALL = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
